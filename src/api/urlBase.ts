0308d1fde9fcf26342cb6e6d99ac3a45edd6460b
rfajardo87 <rfaiard@gmail.com>
2024-08-14 23:29:11 -0600

    configurar variable de ambiente hacia el servidor

--- src/api/urlBase.ts
+++ src/api/urlBase.ts
@@ -1,11 +1,12 @@
-const localServer = `${import.meta.env.VITE_SERVER}${
-  import.meta.env.VITE_PANTRY
-}`;
+import value from "./config";
+
+// cada servicio debe configurar su URL
+const localServer = `https://getpantry.cloud/apiv1/pantry/${value}`;
 
 const basket = (endpoint: string) => `/basket/${endpoint}`;
 
-const GOOGLE_ANALITICS = `${localServer}${basket('demo_analytics')}`;
-const GOOGLE_ADS = `${localServer}${basket('demo_google')}`;
+const GOOGLE_ANALITICS = `${localServer}${basket("demo_analytics")}`;
+const GOOGLE_ADS = `${localServer}${basket("demo_google")}`;
 const META_ADS = `${localServer}${basket("demo_meta")}`;
 const CRM_DATA = `${localServer}${basket("demo_crm")}`;
 
